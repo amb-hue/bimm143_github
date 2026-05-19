ea1c644c07cdf9335f1568a2cf0e5610398891aa
alexia melendez <amelendezbustos@ucsd.edu>
2026-05-19 12:45:38 -0700

    Add class 4

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class4: Intro to R"
+#' author: "Alexia (PID: A17297003)"
+#' date: "April 09, 2026"
+#' ---
+
+# This is just a text file
+x <- 1:50
+plot(x, sin(x), type="l", col="blue", lwd=3)
